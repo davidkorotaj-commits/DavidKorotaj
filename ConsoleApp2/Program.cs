f867843594d6bbd2a0d4aa1e58e89a1b3e14d6ce
davidkorotaj-commits <david.korotaj@skole.hr>
2025-10-06 13:51:12 +0200

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,16 @@
+﻿int n = 0;
+int z = 0;
+int zbroj = 0;
+
+Console.WriteLine("Unesite broj n:");
+
+n = int.Parse(Console.ReadLine());
+
+do{
+    if (z % 2 == 0)
+    {
+    zbroj += z;
+    }
+    z++;
+} while (z <= n) ;
+Console.WriteLine("Broj: {0} ", zbroj);
